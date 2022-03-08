a699b27342d12b205050a4125a320808137f5321
Gourang78 <77731037+Gourang78@users.noreply.github.com>
2022-03-08 19:32:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function setup(){
+
+    video = createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas = createCanvas(550,550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+
+}
+
+function draw(){
+
+    background("red");
+
+}
+
+function modelLoaded(){
+
+    console.log("Model has been loaded.");
+    
+}
+
+function gotPoses(results){
+
+    if(results.length > 0){
+
+        console.log(results);
+
+    }
+
+}
